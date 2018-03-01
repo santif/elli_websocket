c4ec69488121b554a48c845a9c73e6277e4e11c7
Eric Bailey <eric@ericb.me>
2018-03-01 04:21:13 -0600

    elli_websocket: cosmetic tweaks

--- src/elli_websocket.erl
+++ src/elli_websocket.erl
@@ -38,27 +38,26 @@
 
 -type payload() :: binary() | iolist().
 
--type message() ::
-	{text, payload()} |
-	{binary, payload()} |
-	{ping, payload()} |
-	{pong, payload()}.
+-type message() :: {text, payload()} |
+                   {binary, payload()} |
+                   {ping, payload()} |
+                   {pong, payload()}.
 
--type event() :: 
-	websocket_open | websocket_close |
-	websocket_throw | websocket_error | websocket_exit.
+-type event() ::
+        websocket_open | websocket_close |
+        websocket_throw | websocket_error | websocket_exit.
 
 
-%    Args: proplist with settings.
-%            handler: Websocket callback module
-%            handler_opts: Options to pass to the websocket_init 
-%                callback.
-%            resp_compress: bool(), when set to true the traffic 
-%                will be compressed if the client supports it.
-%
+%%    Args: proplist with settings.
+%%            handler: Websocket callback module
+%%            handler_opts: Options to pass to the websocket_init
+%%                callback.
+%%            resp_compress: bool(), when set to true the traffic
+%%                will be compressed if the client supports it.
+%%
 
-% @doc Upgrade the request to a websocket, will respond with 
-% bad request when something is wrong.
+%% @doc Upgrade the request to a websocket, will respond with
+%% bad request when something is wrong.
 -spec upgrade(Req :: elli:req(), list()) -> ok.
 upgrade(Req, Args) ->
     RespCompress = proplists:get_value(resp_compress, Args, false),
@@ -69,4 +68,3 @@ upgrade(Req, Args) ->
     %% Adapter is ready, hand over to ws_protocol
     _UpgradeResponse = elli_ws_protocol:upgrade(ReqAdapter, Args, Handler, HandlerOpts),
     ok.
-
