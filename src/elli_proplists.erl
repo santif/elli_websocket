7bd640bc82d49eed53c916a8b149b8174906188a
Eric Bailey <eric@ericb.me>
2018-03-01 04:12:43 -0600

    elli_proplists: re-indent

--- src/elli_proplists.erl
+++ src/elli_proplists.erl
@@ -4,19 +4,19 @@
 
 -module(elli_proplists).
 
--export([get_value_ci/2, 
-    get_all_values_ci/2]).
+-export([get_value_ci/2,
+         get_all_values_ci/2]).
 
 %%
 get_value_ci(_Key, []) ->
-        undefined;
+    undefined;
 get_value_ci(Key, [{K, Value}|Rest]) ->
-        case elli_bstr:is_equal_ci(Key, K) of
-                true ->
-                        Value;
-                false ->
-                        get_value_ci(Key, Rest)
-        end.
+    case elli_bstr:is_equal_ci(Key, K) of
+        true ->
+            Value;
+        false ->
+            get_value_ci(Key, Rest)
+    end.
 
 
 %%
@@ -25,11 +25,11 @@ get_all_values_ci(Key, Proplist) ->
 
 
 get_all_values_ci1(_Key, [], Acc) ->
-        lists:reverse(Acc);
+    lists:reverse(Acc);
 get_all_values_ci1(Key, [{K, Value}|Rest], Acc) ->
-        case elli_bstr:is_equal_ci(Key, K) of
-                true ->
-                        get_all_values_ci1(Key, Rest, [Value|Acc]);
-                false ->
-                        get_all_values_ci1(Key, Rest, Acc)
-        end.
+    case elli_bstr:is_equal_ci(Key, K) of
+        true ->
+            get_all_values_ci1(Key, Rest, [Value|Acc]);
+        false ->
+            get_all_values_ci1(Key, Rest, Acc)
+    end.
