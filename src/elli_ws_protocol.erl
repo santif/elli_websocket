026ca50267e9d4959612b20836fe26aabba64d86
Santiago Fernandez <sfernandez@matriz.io>
2018-07-22 16:07:51 -0300

    Replace `crypto:sha/1` with `crypto:hash/2`

--- src/elli_ws_protocol.erl
+++ src/elli_ws_protocol.erl
@@ -185,8 +185,7 @@ handler_init(State=#state{env=Env, handler=Handler}, Req, HandlerOpts) ->
 %%   | {suspend, module(), atom(), [any()]}
 %%   when Req::elli_ws_request_adapter:req().
 websocket_handshake(State=#state{key=Key, deflate_frame=DeflateFrame, handler=Handler}, Req, HandlerState) ->
-    %% @todo Change into crypto:hash/2 for R17B+ or when supporting only R16B+.
-    Challenge = base64:encode(crypto:sha(
+    Challenge = base64:encode(crypto:hash(sha,
                                 << Key/binary, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" >>)),
     Extensions = case DeflateFrame of
                      false -> [];
